b2818008676ee62d9f1fa3a5e68f583e555b11cb
Chesley Tan <chesleytan97@gmail.com>
2014-12-22 11:38:59 -0500

    Read session key from file

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from functools import wraps
 
 app = Flask(__name__)
 
-
-
-# Tmp, make an actual one when we deploy
-app.secret_key = '\x90\x9c\xe3C<\x12]^v0p\xde\xc7\xb2\xa1\xea\x90e\x10\xfe\xf1\xd0\xa7g'
+app.secret_key = open('session_key.txt', 'r').read().strip()
 
 if __name__ == "__main__":
     app.debug=True
