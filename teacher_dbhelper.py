cf9c42e54e6d8fdb86f7740a44426779732b47cd
ChesleyTan <ChesleyTan97@gmail.com>
2014-12-30 16:51:14 -0500

    Add teacher database helper
    Use single quotes for identifiers/keys

--- teacher_dbhelper.py
+++ teacher_dbhelper.py
@@ -2,10 +2,10 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.gradebook
-students = db.students
+teachers = db.teachers
 
 # TODO implement validation in validation.py
-def insert(email, password, name):
+def insert(email, password, name, school):
     # TODO hash & salt
     # TODO validation for each field
     # TODO email confirmation
@@ -14,9 +14,10 @@ def insert(email, password, name):
             'email' : email,
             'password' : password,
             'name' : name,
+            'school' : school,
             'classes' : []
         }
-        students.insert(new_user)
+        teachers.insert(new_user)
         return (True, "Registration successful. Enjoy!")
     else:
         return (False, "Registration error: A user with that email already\
@@ -32,25 +33,27 @@ def validate_registration(email, password):
     return (True, "Registration successful. Enjoy!")
 
 def exists(email):
-    return students.find({'email' : email}).count() > 0
+    return teachers.find({'email' : email}).count() > 0
 
 def remove(email):
-    students.remove({'email' : email}, multi=False)
+    teachers.remove({'email' : email}, multi=False)
 
 def validate_login(email, password):
-    return students.find({'email' : email, 'password' : password}).count() == 1
+    return teachers.find({'email' : email, 'password' : password}).count() == 1
 
 def get(email):
-    return students.find({'email': email})
+    return teachers.find({'email': email})
 
-def update(email, new_email=None, name=None, password=None, classes=None):
+def update(email, new_email=None, name=None, school=None, password=None,
+           classes=None):
     if(exists(email)):
         updateDict = {}
         if new_email != None: updateDict['email'] = new_email
         if name != None: updateDict['name'] = name
+        if school != None: updateDict['school'] = school
         if password != None: updateDict['password'] = password
         if classes != None: updateDict['classes'] = classes
-        students.update(
+        teachers.update(
             {'email': email},
                 {'$set': updateDict}
         )
@@ -59,19 +62,21 @@ def update(email, new_email=None, name=None, password=None, classes=None):
         return "Update info error: User doesn't exist!"
 
 def dump():
-    for c in students.find():
+    for c in teachers.find():
         print c
 
 def drop():
-    students.remove()
+    teachers.remove()
 
 ########## TESTING ##########
 if __name__ == "__main__":
     drop()
-    response_tuple = insert("john@gmail.com", "p4$$w0rD", "John Doe")
+    response_tuple = insert("john@gmail.com", "p4$$w0rD", "John Doe",
+                            "Stuyvesant High School")
     if not response_tuple[0]:
         print response_tuple[1]
     dump()
-    update("john@gmail.com", name="Anonymous", new_email="anonymous@gmail.com")
+    update("john@gmail.com", name="Anonymous", new_email="anonymous@gmail.com",
+            school="Hogwarts School of Witchcraft and Wizardry")
     dump()
 
