21f4a24ee26d63679ba03e1c8369ad34976995fe
Chesley Tan <chesleytan97@gmail.com>
2015-01-20 15:45:18 -0500

    Add validation to teacher_dbhelper.py

--- teacher_dbhelper.py
+++ teacher_dbhelper.py
@@ -1,36 +1,36 @@
 from pymongo import MongoClient
+from validation import *
 
 client = MongoClient()
 db = client.gradebook
 teachers = db.teachers
 
-# TODO implement validation in validation.py
+# TODO email confirmation
 def insert(email, password, name, school):
-    # TODO hash & salt
-    # TODO validation for each field
-    # TODO email confirmation
+    response_tuple = isValidEmail(email)
+    if not response_tuple[0]:
+        return response_tuple
+    response_tuple = isValidPassword(password)
+    if not response_tuple[0]:
+        return response_tuple
+    response_tuple = isValidName(name)
+    if not response_tuple[0]:
+        return response_tuple
+    response_tuple = isValidSchool(school)
+    if not response_tuple[0]:
+        return response_tuple
     if (not exists(email)):
         new_user = {
             'email' : email,
-            'password' : password,
+            'password' : generatePasswordHash(password),
             'name' : name,
             'school' : school,
             'courses' : []
         }
         teachers.insert(new_user)
-        return (True, "Registration successful. Enjoy!")
+        return (True, "Successfully registered. Enjoy!")
     else:
-        return (False, "Registration error: A user with that email already "
-                "exists.")
-
-def validate_registration(email, password):
-    if (len(email) < 1):
-        return (False, "Registration error: Your email must be at least 1 "
-                "character long")
-    if (len(password) < 6):
-        return (False, "Registration error: Your password must be at least 6 "
-                "characters long")
-    return (True, "Registration successful. Enjoy!")
+        return (False, "Error: A user with that email already exists.")
 
 def exists(email):
     return teachers.find({'email' : email}).count() > 0
@@ -38,20 +38,34 @@ def exists(email):
 def remove(email):
     teachers.remove({'email' : email}, multi=False)
 
-def validate_login(email, password):
-    return teachers.find({'email' : email, 'password' : password}).count() == 1
-
 def get(email):
     return teachers.find({'email': email})
 
 def update(email, new_email=None, name=None, school=None, password=None,
            courses=None):
+    if new_email:
+        response_tuple = isValidEmail(new_email)
+        if not response_tuple[0]:
+            return response_tuple
+    if password:
+        response_tuple = isValidPassword(password)
+        if not response_tuple[0]:
+            return response_tuple
+    if name:
+        response_tuple = isValidName(name)
+        if not response_tuple[0]:
+            return response_tuple
+    if school:
+        response_tuple = isValidSchool(school)
+        if not response_tuple[0]:
+            return response_tuple
     if(exists(email)):
         updateDict = {}
         if new_email != None: updateDict['email'] = new_email
         if name != None: updateDict['name'] = name
         if school != None: updateDict['school'] = school
-        if password != None: updateDict['password'] = password
+        if password != None: updateDict['password'] =\
+                             generatePasswordHash(password)
         if courses != None: updateDict['courses'] = courses
         teachers.update(
             {'email': email},
@@ -59,7 +73,7 @@ def update(email, new_email=None, name=None, school=None, password=None,
         )
         return "Successfully updated info."
     else:
-        return "Update info error: User doesn't exist!"
+        return "Error: User doesn't exist!"
 
 def dump():
     for c in teachers.find():
--- validation.py
+++ validation.py
@@ -1,4 +1,5 @@
 from validate_email import validate_email
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def isValidEmail(email):
     valid = validate_email(email)
@@ -12,8 +13,7 @@ def isValidName(name):
     if not valid:
         return (False, "Error: Name is too long (Greater than 30 characters)")
     valid = len(name) > 0
-    if not valid:
-        return (False, "Error: Name is too short (Fewer than 1 character)")
+    if not valid: return (False, "Error: Name is too short (Fewer than 1 character)")
     return (True, "Successfully validated name")
 
 def isValidPassword(password):
@@ -34,7 +34,7 @@ def isValidSchool(school):
     if not valid:
         return (False, "Error: Name of school is too long (Greater than 100"
                 " characters)")
-    valid = isupper(school[0])
+    valid = school[0].isupper()
     if not valid:
         return (False, "Error: Name of school must be capitalized")
     return (True, "Successfully validated school name")
@@ -43,6 +43,22 @@ def isValidClass(className):
     valid = len(className) > 1
     if not valid:
         return (False, "Error: Name of class is too short (Fewer than 2"
-                " characters")
-    # TODO
+                " characters)")
+    valid = len(className) < 31
+    if not valid:
+        return (False, "Error: Name of class is too long (Greater than 30"
+                " characters)")
+    return (True, "Successfully validated class name")
 
+def isValidDescription(description):
+    valid = len(description) < 301
+    if not valid:
+        return (False, "Error: Description is too long (Greater than 300"
+                " characters)")
+    return (True, "Successfully validated description")
+
+def checkPassword(hashedPassword, tryPassword):
+    return check_password_hash(hashedPassword, tryPassword)
+
+def generatePasswordHash(password):
+    return generate_password_hash(password)
