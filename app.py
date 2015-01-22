abb547b90fd0bc85b49dda6410781a1fb9ff781b
Eric Wong <wonggeric@gmail.com>
2015-01-22 11:48:54 -0500

    Remember me

--- app.py
+++ app.py
@@ -14,20 +14,41 @@ def about():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+
+    if 'email' in session and rmber:
+        if escape(session['utype'])=='teacher':
+        return render_template("teacher.html", email, pword)
+    elif method='POST':
+        email = request.form['email']
+        pword = request.form['password']
+        name = request.form['name']
+        utype = request.form['usertype']
+        if utype=='teacher':
+            school = request.form['school']
+
     register = request.form['register']
     submit = request.form['submit']
-    email = request.form['mail']
-    pword = request.form['password']
     
-    if register and submit and email and pword:
+    if register:
+        if utype=='teacher':
+            if insert(email, pword, name, ):
+            flash("You have successfully register for an account")
+        else:
+            flash("Email or password are already in use")
+        return render_template("login.html")
+    elif submit:
+        if validate(email, pword):
+            return redirect(url_for(
+            
+        
         # TODO
         pass
     return render_template("login.html")
 
 @app.route("/logout")
 def logout():
-    session.pop('USER',None)
-    return redirect(url_for('home')
+    session.pop('email',None)
+    return redirect(url_for('home'))
 
 @app.route("/user")
 def user():
