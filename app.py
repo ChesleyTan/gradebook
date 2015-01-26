f41a018fb645d6372d6097a0ed0cc635e3ed6e23
eshoyfer <eshoyfer@gmail.com>
2015-01-26 18:42:16 -0500

    Split the POST component of message_page in app.py to check for message submission in session

--- app.py
+++ app.py
@@ -648,17 +648,29 @@ def message_page(user=None):
                 return render_template('message_page.html', isTeacher=True,
                             teacher_data=teacher[0], target=user)
     else:
-        if session['userType'] == 'student':
-            student = studentdb.get(session['email'])
-            if student.count() == 1:
-                return render_template('message_page.html', isStudent=True,
-                            student_data=student[0], target=user)
-        else:
-            teacher = teacherdb.get(session['email'])
-            if teacher.count() == 1:
-                return render_template('message_page.html', isTeacher=True,
-                            teacher_data=teacher[0], target=user)
-                
+        if 'submit' in session: # A message was sent
+            if session['userType'] == 'student':
+                student = studentdb.get(session['email'])
+                if student.count() == 1:
+                    return render_template('message_page.html', isStudent=True,
+                                student_data=student[0], target=user)
+            else:
+                teacher = teacherdb.get(session['email'])
+                if teacher.count() == 1:
+                    return render_template('message_page.html', isTeacher=True,
+                                teacher_data=teacher[0], target=user)
+        else: # Message not send
+            if session['userType'] == 'student':
+                student = studentdb.get(session['email'])
+                if student.count() == 1:
+                    return render_template('message_page.html', isStudent=True,
+                                student_data=student[0], target=user)
+            else:
+                teacher = teacherdb.get(session['email'])
+                if teacher.count() == 1:
+                    return render_template('message_page.html', isTeacher=True,
+                                teacher_data=teacher[0], target=user)
+                                              
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
