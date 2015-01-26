65dc0b17408d762fdbb721dfc82ce937e436fe79
eshoyfer <eshoyfer@gmail.com>
2015-01-26 01:33:36 -0500

    Messages page - unvariabled front end template

--- app.py
+++ app.py
@@ -499,7 +499,7 @@ def course(course_id=None):
 
 @app.route('/messages')
 @redirect_if_not_logged_in
-def messages():
+def messages(): 
     if session['userType'] == 'student':
         student = studentdb.get(session['email'])
         if student.count() == 1:
