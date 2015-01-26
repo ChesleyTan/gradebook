19a8a696b0ff5da6c98ac7689efd2b3e485ec239
eshoyfer <eshoyfer@gmail.com>
2015-01-26 02:09:36 -0500

    Returning HTTP error status codes along with the HTML for errors

--- app.py
+++ app.py
@@ -619,11 +619,11 @@ def message_page():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html')
+    return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template('500.html')
+    return render_template('500.html'), 500
 
 #======================END-DEFINITIONS======================
 
